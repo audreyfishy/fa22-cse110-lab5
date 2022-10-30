ca21955c3ff531b4f6eaa266d93f889d57538430
audreyfishy <kitazawatakuro@gmail.com>
2022-10-29 18:33:23 -0700

    Everyting done except 2-2

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -2,6 +2,66 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+function updateImage(isSpeaking) {
+  let image = document.querySelector('img');
+  if(isSpeaking) {
+    image.src = 'assets/images/smiling-open.png';
+    image.alt = 'Smiling open';
+  }
+  else{
+    image.src = 'assets/images/smiling.png';
+    image.alt = 'Smiling face';
+  }
+}
+
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+
+  const inputForm = document.getElementById('text-to-speak');
+  const voiceSelect = document.querySelector('select');
+
+  let voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+
+    for (let i = 0; i < voices.length ; i++) {
+      const option = document.createElement('option');
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += ' — DEFAULT';
+      }
+
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  const button = document.querySelector('button');
+  button.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const utterThis = new SpeechSynthesisUtterance(inputForm.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for (let i = 0; i < voices.length ; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    updateImage(true);
+    synth.speak(utterThis);
+    var timer = setInterval(() => {
+      if(synth.speaking == false) {
+        updateImage(false);
+        clearInterval(timer);
+      }
+    }, 1000);
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,66 @@
-// expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+function updateHorn(horn){
+  var hornImage = document.getElementById("expose").children[1];
+  var hornAudio = document.getElementsByClassName("hidden")[0];
+  hornImage.src = `assets/images/${horn}.svg`;
+  hornImage.alt = `${horn} image`;
+  hornAudio.src = `assets/audio/${horn}.mp3`;
+  hornAudio.alt = `${horn} sound`;
+}
+
+function updateVolumeImage(volume){
+  var volumeImage = document.getElementById("volume-controls").children[1];
+  if (volume >= 67){
+    volumeImage.src = "assets/icons/volume-level-3.svg";
+    volumeImage.alt = "Volume level 3";
+  }
+  else if (volume >= 33){
+    volumeImage.src = "assets/icons/volume-level-2.svg";
+    volumeImage.alt = "Volume level 2";
+  }
+  else if (volume >= 1){
+    volumeImage.src = "assets/icons/volume-level-1.svg";
+    volumeImage.alt = "Volume level 1";
+  }
+  else{
+    volumeImage.src = "assets/icons/volume-level-0.svg";
+    volumeImage.alt = "Volume level 0";
+  }
+}
+
+function updateVolume(volume){
+  let hornAudio = document.getElementsByClassName("hidden")[0];
+  hornAudio.volume = volume / 100;
+}
 
 function init() {
-  // TODO
+  // Add event listener to the drop down menu
+  const hornSelect = document.getElementById("horn-select");
+  hornSelect.addEventListener('input', (event) => {
+    // Get the value of the selected option
+    const selectedHorn = event.target.value;
+    // Update the image and audio sources
+    updateHorn(selectedHorn);
+  });
+
+  // Add event listener to the volume slider
+  const volumeSlider = document.getElementById("volume");
+  volumeSlider.addEventListener('input', (event) => {
+    // Get the value of the volume slider
+    const volume = event.target.value;
+    // Update the volume image
+    updateVolumeImage(volume);
+    // Update the volume of the <audio> element
+    updateVolume(volume);
+  });
+
+  // Add event listener to the play button
+  const audio = document.getElementsByClassName("hidden")[0];
+  const playButton = audio.previousElementSibling;
+  playButton.addEventListener('click', (event) => {
+    audio.play();
+    const jsConfetti = new JSConfetti()
+    if(hornSelect.value == "party-horn") jsConfetti.addConfetti();
+  });
 }
+window.addEventListener('DOMContentLoaded', init);
